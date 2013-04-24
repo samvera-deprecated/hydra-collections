e5cea20d9f7b6c41851e3eff940c054a5247d5af
Carolyn Cole <cam156@psu.edu>
2013-04-24 11:01:40 -0400

    Renaming add_collection_button to button_create_collecton and adding in spec to test helper

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -3,8 +3,8 @@ module CollectionsHelper
   
  
   # Displays the Collections create collection button.  Put this in your search result page template.  We recommend putting it in catalog/_sort_and_per_page.html.erb
-  def collections_add_collection(label = 'Create Collection')
-    render :partial=>'/collections/add_collection_button', :locals=>{:label=>label}
+  def button_create_collection(label = 'Create Collection')
+    render :partial=>'/collections/button_create_collection', :locals=>{:label=>label}
   end
   
 end
---
+++ spec/helpers/collections_helper_spec.rb
@@ -0,0 +1,29 @@
+# Copyright © 2013 The Pennsylvania State University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'spec_helper'
+
+include Hydra::Collections::Engine.routes.url_helpers
+
+describe CollectionsHelper do
+  it "button_create_collection should create a button to the collections new path" do
+    html = button_create_collection 
+    html.should have_selector("form[action='#{collections.new_collection_path}']")
+    html.should have_selector("input[type='submit']")
+  end
+  it "button_create_collection should create a button with my text" do
+    html = button_create_collection "Create My Button"
+    html.should have_selector("input[value='Create My Button']")
+  end
+end

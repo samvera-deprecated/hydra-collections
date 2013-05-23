9155f2f398d5e1329bb5864e2a4fa82eb2ddcf92
Carolyn Cole <cam156@psu.edu>
2013-05-23 08:47:23 -0400

    Make sure that each id is only added to the form once. Bumping the version to 0.0.2

--- app/assets/javascripts/hydra/batch_select.js
+++ app/assets/javascripts/hydra/batch_select.js
@@ -32,7 +32,8 @@ $(function () {
       var form = $(this).closest("form"); 
       $.map( $(".batch_document_selector:checked"), function(document, i) {
          var id = document.id.substring("batch_document_".length);
-        form.append('<input type="hidden" multiple="multiple" name="batch_document_ids[]" value="'+id+'" />');
+         if (form.children("input[value='"+id+"']").length == 0)
+           form.append('<input type="hidden" multiple="multiple" name="batch_document_ids[]" value="'+id+'" />');
       });
   
       

7ea189e15e06006788b89257f98539b7829fc703
Justin Coyne <jcoyne@justincoyne.com>
2016-03-04 11:19:18 -0600

    Move title to RequiredMetadata

--- app/models/concerns/hydra/collections/required_metadata.rb
+++ app/models/concerns/hydra/collections/required_metadata.rb
@@ -2,6 +2,10 @@ module Hydra::Collections
   module RequiredMetadata
     extend ActiveSupport::Concern
     included do
+      property :title, predicate: RDF::Vocab::DC.title do |index|
+        index.as :stored_searchable, :sortable
+      end
+
       property :depositor, predicate: RDF::Vocab::MARCRelators.dpt, multiple: false do |index|
         index.as :symbol, :stored_searchable
       end
